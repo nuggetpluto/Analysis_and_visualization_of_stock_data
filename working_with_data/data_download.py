9b4403fa020e1f633186ce8153a0bec9ec5001c0
nuggetpluto <korshikov1501@gmail.com>
2024-12-03 20:17:37 +0300

    Реализован функционал: Вывод средней цены за период

---
+++ working_with_data/data_download.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+
+
+def fetch_stock_data(ticker, period='1mo'):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
+
+
+def add_moving_average(data, window_size=5):
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
+    return data
+
+
+def calculate_and_display_average_price(data):
+    """
+    Вычисляет и выводит среднюю цену закрытия за заданный период.
+    :param data: DataFrame с колонкой 'Close'
+    """
+    if 'Close' in data.columns:
+        average_price = data['Close'].mean()
+        print(f"Средняя цена закрытия за период: {average_price:.2f}")
+    else:
+        print("Колонка 'Close' отсутствует в данных.")
